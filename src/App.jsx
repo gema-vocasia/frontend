910ab914279074f20297a3d6ae06d7164373304f
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-11-29 17:40:44 +0700

    awalan membuat halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Membuat Halaman Register</h1>
+      <h1>Membuat Halaman Login</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
