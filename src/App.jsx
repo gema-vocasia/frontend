b36a83931d6c7dcef30504275b7e9a77ba19390e
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-11-29 17:47:52 +0700

    awalan membuat halaman campaign saya

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Membuat Halaman Riwayat</h1>
+      <h1>Membuat Halaman Campaign Saya</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
