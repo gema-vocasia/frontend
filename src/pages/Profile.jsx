3f79736d51fd81b2c02404f8382d72ecd72f04cd
kunyitt <trimlyni15@gmail.com>
2024-11-30 10:16:11 +0700

    membuat profile content di halaman profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import ProfileContent from '../components/ProfileContent';
+
+const Profile = () => {
+
+  return (
+    <div className="relative min-h-screen bg-white flex flex-col lg:flex-row">
+      <ProfileContent />
+    </div>
+  );
+};
+
+export default Profile;
+
