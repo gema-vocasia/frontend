b33390f38bf2e65136ee100fe9648077bd3e970c
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-12-02 16:04:07 +0700

    membuat halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,111 @@
-import { useState } from "react";
-import "./App.css";
+// import { useState } from "react";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Membuat Halaman Login</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      {/* <h1>Membuat Halaman Login</h1> */}
+      <main className="flex items-center justify-center min-h-screen">
+        <card className=" w-full max-w-md p-7 rounded-lg shadow-md">
+          <header className=" mb-4">
+            <h2 className="text-center text-2xl font-semibold text-black">
+              Edit Profile
+            </h2>
+          </header>
+          <section>
+            <div
+              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-5"
+              aria-label="Profile Picture"
+            ></div>
+            <form className="space-y-3">
+              <div className="text-left">
+                <label
+                  htmlFor="full-name"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Nama Lengkap
+                </label>
+                <input
+                  type="text"
+                  id="full-name"
+                  name="full-name"
+                  placeholder="Nama Lengkap"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+                />
+              </div>
+
+              <div className="text-left">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+                />
+              </div>
+
+              <div className="text-left">
+                <label
+                  htmlFor="phone"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  No Telepon
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="No Telepon"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+                />
+              </div>
+
+              <div className="text-left">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+                />
+              </div>
+
+              <div className="flex justify-left gap-3">
+                <button
+                  type="button"
+                  className="px-4 py-2 bg-gray-400 text-white rounded-md text-sm hover:opacity-90"
+                >
+                  Batal
+                </button>
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-blue-500 text-white rounded-md text-sm hover:opacity-90"
+                >
+                  Simpan
+                </button>
+              </div>
+            </form>
+          </section>
+        </card>
+      </main>
     </>
   );
 }
