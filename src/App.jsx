d648c86a0ab551bc90c895dd7cd92ea42559a60e
Muhammad Thoriq Dzulfiqar <muhthoriqdzulfiqar@gmail.com>
2024-12-03 17:25:03 +0700

    edit. app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,101 +6,117 @@ function App() {
 
   return (
     <>
-      {/* <h1>Membuat Halaman Register</h1> */}
-      <main className="flex items-center justify-center min-h-screen">
-        <card className=" w-full max-w-md p-7 rounded-lg shadow-md">
-          <section>
-            <div
-              className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-5"
-              aria-label="Profile Picture"
-            ></div>
-            <form className="space-y-3">
-              <div className="text-left">
-                <label
-                  htmlFor="full-name"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Nama Lengkap
-                </label>
-                <input
-                  type="text"
-                  id="full-name"
-                  name="full-name"
-                  placeholder="Nama Lengkap"
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-                />
-              </div>
-
-              <div className="text-left">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
-                />
-              </div>
-
-              <div className="text-left">
-                <label
-                  htmlFor="phone"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  No Telepon
-                </label>
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  placeholder="No Telepon"
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
-                />
-              </div>
-
-              <div className="text-left">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Password"
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
-                />
-              </div>
-
-              <div className="flex justify-center">
-                <button
-                  type="submit"
-                  className="px-4 py-2 bg-blue-500 text-white rounded-md text-sm hover:opacity-90"
-                >
-                  Sign Up
-                </button>
-              </div>
-              <p className="text-center">
-                Already Have An Account ?
-                <a href="#" className="text-blue-500">
-                  Login
-                </a>
-              </p>
-            </form>
-          </section>
+      <main className="flex items-center justify-center min-h-screen bg-grey-200">
+        <card className=" w-full max-w-md p-7 rounded-lg shadow-md bg-white shadow-xl">
+          <ProfilePicture />
+          <FormRegistrasi />
         </card>
       </main>
+    </>
+  );
+}
+
+function ProfilePicture() {
+  return (
+    <>
+      <h1 className="text-2xl mb-4 text-center font-bold">
+        WELLCOME IN GEMA WEBSITE
+      </h1>
+      <div
+        className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-5"
+        aria-label="Profile Picture"
+      ></div>
+    </>
+  );
+}
+
+function FormRegistrasi() {
+  return (
+    <>
+      <form className="space-y-3">
+        <div className="text-left">
+          <label
+            htmlFor="full-name"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Nama Lengkap
+          </label>
+          <input
+            type="text"
+            id="full-name"
+            name="full-name"
+            placeholder="Nama Lengkap"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+          />
+        </div>
+
+        <div className="text-left">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+          />
+        </div>
+
+        <div className="text-left">
+          <label
+            htmlFor="phone"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            No Telepon
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="No Telepon"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+          />
+        </div>
+
+        <div className="text-left">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            required
+            className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm "
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded-md text-sm hover:opacity-90"
+          >
+            Sign Up
+          </button>
+        </div>
+        <p className="text-center">
+          Already Have An Account ?
+          <a href="#" className="text-blue-500">
+            Login
+          </a>
+        </p>
+      </form>
     </>
   );
 }
