fb8c61257a14e8d06dab165e5a5b3d60bee89be2
azizah89 <azizahpauziah716@gmail.com>
2024-12-04 23:01:14 +0700

    Menampilkan tampilan All Kampanye

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TentangKami from "./pages/TentangKami";
-import Home from "./pages/Home"; // Import komponen Home
+import Home from "./pages/Home";
+import AllKampanye from "./pages/AllKampanye"; // Import komponen AllKampanye
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Tambahkan route untuk Home */}
-        <Route path="/TentangKami" element={<TentangKami />} />
+        <Route path="/" element={<Home />} /> {/* Route untuk Home */}
+        <Route path="/TentangKami" element={<TentangKami />} /> {/* Route untuk TentangKami */}
+        <Route path="/all-kampanye" element={<AllKampanye />} /> {/* Route untuk AllKampanye */}
       </Routes>
     </BrowserRouter>    
   );
