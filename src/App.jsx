61337e71a92c47a9e6c1003ee08adff748eef658
kunyitt <trimlyni15@gmail.com>
2024-12-04 23:07:56 +0700

    clear main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Profile from "./pages/Profile";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>    
+    <>
+    </>   
   );
 }
 
