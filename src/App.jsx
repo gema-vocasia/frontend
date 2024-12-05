8a9fa534376d5026efb92e34cf58608cd595ff61
azizah89 <azizahpauziah716@gmail.com>
2024-12-05 23:49:43 +0700

    Menambahkan tampilan all campaign

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Donasi from "./pages/Donasi";
-
+import AllKampanye from "./pages/AllKampanye";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/donasi" element={<Donasi />} />
+      <Route path="/all-kampanye" element={<AllKampanye />} /> {/* Route untuk AllKampanye */}
       </Routes>
     </BrowserRouter>    
   );
