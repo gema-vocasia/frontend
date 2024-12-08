f91f818cd8046e71c3ad624fb3d41946f9e0f7a5
kunyitt <trimlyni15@gmail.com>
2024-12-08 20:35:06 +0700

    add path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BuatCampaign from "./pages/BuatCampaign";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile"
+import Donasi from "./pages/Donasi";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/buat-campaign" element={<BuatCampaign />} />
+        <Route path="/donasi" element={<Donasi />} />
       </Routes>
     </BrowserRouter>    
   );
