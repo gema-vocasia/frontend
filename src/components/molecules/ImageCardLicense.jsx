0bd33a1de3bb694c9fb0a203ddecfd87fc430e07
azizah89 <azizahpauziah716@gmail.com>
2024-12-14 11:27:34 +0700

    Memperbarui tampilan halaman tentang kami

--- src/components/molecules/ImageCardLicense.jsx
+++ src/components/molecules/ImageCardLicense.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Image from '../atoms/Image';
 
-const ImageCard = ({ image, onClick }) => (
+const ImageCardLicense = ({ image, onClick }) => (
   <div
     className="relative overflow-hidden transition-all duration-300 transform bg-white shadow-lg rounded-2xl hover:shadow-2xl hover:scale-105 hover:bg-indigo-50"
     onClick={onClick}
   >
-    <Image src={image.src} alt={image.owner} className="object-cover w-full h-40 rounded-t-2xl" />
+    {/* Gambar dengan ukuran lebih besar */}
+    <Image src={image.src} alt={image.owner} className="object-cover w-full h-60 rounded-t-2xl" />
+
+    {/* Informasi Pemilik */}
     <div className="absolute bottom-0 left-0 right-0 p-2 text-sm text-white bg-black bg-opacity-50">
       <p>
         <span className="font-medium">Pemilik:</span> {image.owner}
@@ -15,4 +18,5 @@ const ImageCard = ({ image, onClick }) => (
   </div>
 );
 
-export default ImageCard;
+export default ImageCardLicense;
+
